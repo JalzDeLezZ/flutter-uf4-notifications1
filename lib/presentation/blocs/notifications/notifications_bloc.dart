e6cb70b71dd897a27ab830776599227450edf828
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-08-18 00:57:16 -0500

    [add] :: get token device

--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -13,6 +15,8 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
 
   NotificationsBloc() : super(const NotificationsState()) {
     on<NotificationStatusChanged>(_notificationStatusChanges);
+
+    _initialStatusCheck();
   }
 
   static Future<void> initFCM() async {
@@ -23,6 +27,20 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
 
   void _notificationStatusChanges(event, emit) {
     emit(state.copyWith(status: event.status));
+    _getFCMToken();
+  }
+
+  void _initialStatusCheck() async {
+    final settings = await messaging.getNotificationSettings();
+    add(NotificationStatusChanged(settings.authorizationStatus));
+  }
+
+  void _getFCMToken() async {
+    final settings = await messaging.getNotificationSettings();
+    if (state.status != AuthorizationStatus.authorized) return;
+    // if (settings.authorizationStatus != AuthorizationStatus.authorized) return;
+    final token = await messaging.getToken();
+    print('FCM Token: $token');
   }
 
   void requestPermission() async {
